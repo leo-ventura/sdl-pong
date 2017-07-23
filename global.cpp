35f941732df99c0474a4b8097d142d691637c64f
Leonardo Ventura <leonardo.ventura98@icloud.com>
2017-07-22 23:44:39 -0300

    removed duplicated files

--- global.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <SDL2/SDL.h>
-#include "global.h"
-
-bool gQuit = false;
-SDL_Surface *gScreenSurface = NULL;
-SDL_Window *gWindow = NULL;
-SDL_Surface* gBarSurface = NULL;
-SDL_Surface *gBallSurface = NULL;
