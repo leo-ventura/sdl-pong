83c5c48bfe2007f9ed81fbd82d099ea023ce1f59
Leonardo Ventura <leonardo.ventura98@icloud.com>
2017-07-26 01:14:09 -0300

    trying to make ttf work :baby_symbol:

--- src/functions.cpp
+++ src/functions.cpp
@@ -63,7 +63,7 @@ SDL_Surface *getMedia(string pwd) {
   return optimizedSurface;
 }
 
-  bool loadMedia() {
+bool loadMedia() {
 
   gBarSurface = getMedia("../images/bar.png");
   gBallSurface = getMedia("../images/ball.png");
@@ -92,9 +92,38 @@ void closeAll() {
   SDL_Quit();
 }
 
-void putDimensions(SDL_Rect* src, int srcx, int srcy, int srcw, int srch, SDL_Rect* dst, int dstx, int dsty) {
-  src->x = srcx;
-  src->y = srcy;
+void loadFont(TTF_Font* font, int size) {
+  font = TTF_OpenFont("../images/alagard_BitFont.ttf", size);
+  if (!font) {
+    cout << "Failed to load font! Error: " << TTF_GetError() << endl;
+    gQuit = true;
+  }
+  if(_DEBUG) cout << "[Loaded font]" << endl;
+}
+
+SDL_Surface* loadText(TTF_Font* font, string text, SDL_Color color) {
+
+  SDL_Surface *optimizedTextSurface;
+
+  SDL_Surface *textSurface = TTF_RenderText_Solid(font, text.c_str(), color);
+  if (!textSurface) {
+    cout << "Unable to render text. Error: " << TTF_GetError() << endl;
+    gQuit = true;
+  }
+
+  optimizedTextSurface = SDL_ConvertSurface(textSurface, gScreenSurface->format, 0);
+  if (!optimizedTextSurface) {
+    cout << "Unable to create texture. Error: " << TTF_GetError() << endl;
+    gQuit = true;
+  }
+  SDL_FreeSurface(textSurface);
+
+  return optimizedTextSurface;
+}
+
+void putDimensions(SDL_Rect* src, int srcw, int srch, SDL_Rect* dst, int dstx, int dsty) {
+  src->x = 0;
+  src->y = 0;
   src->w = srcw;
   src->h = srch;
   dst->x = dstx;
@@ -116,7 +145,7 @@ void startGame() {
   // declaring left bar, right bar and ball
   Bar leftBar(30, WINDOW_HEIGHT/2, BAR_WIDTH, BAR_HEIGHT, gBarSurface);
   Bar rightBar(WINDOW_WIDTH - BAR_WIDTH - 30, WINDOW_HEIGHT/2, BAR_WIDTH, BAR_HEIGHT, gBarSurface);
-  Ball ball(WINDOW_WIDTH/2, WINDOW_HEIGHT/2, BALL_WIDTH, BALL_HEIGHT, gBallSurface);
+  Ball ball(WINDOW_WIDTH/2 - BALL_WIDTH/2, WINDOW_HEIGHT/2 - BALL_HEIGHT/2, BALL_WIDTH, BALL_HEIGHT, gBallSurface);
 
   // game loop
   while(!gQuit) {
@@ -131,13 +160,11 @@ void startGame() {
           else if (e.key.keysym.sym == SDLK_SPACE) {
             srand(time(NULL));
             if(rand()%2) {
-              ball.setStepX(1);
-              if(rand()%2) ball.setStepY(1);
-              else ball.setStepY(-1);
+              ball.setStepX(2);
+              rand()%2 ? ball.setStepY(1):ball.setStepY(-1);
             } else {
-              ball.setStepX(-1);
-              if(rand()%2) ball.setStepY(1);
-              else ball.setStepY(-1);
+              ball.setStepX(-2);
+              rand()%2 ? ball.setStepY(1):ball.setStepY(-1);
             }
           }
           else if (e.key.keysym.sym == SDLK_w)
@@ -160,14 +187,13 @@ void startGame() {
 
     // putDimensions prototype
     // putDimensions(SDL_Rect* src, int srcx, int srcy, int srcw, int srch, SDL_Rect* dst, int dstx, int dsty);
-    putDimensions(&srcLeftBar, 0, 0, BAR_WIDTH, BAR_HEIGHT, &dstLeftBar, leftBar.getX(), leftBar.getY());
-    putDimensions(&srcRightBar, 0, 0, BAR_WIDTH, BAR_HEIGHT, &dstRightBar, rightBar.getX(), rightBar.getY());
-    putDimensions(&srcBall, 0, 0, BALL_WIDTH, BALL_HEIGHT, &dstBall, ball.getX(), ball.getY());
+    putDimensions(&srcLeftBar, BAR_WIDTH, BAR_HEIGHT, &dstLeftBar, leftBar.getX(), leftBar.getY());
+    putDimensions(&srcRightBar, BAR_WIDTH, BAR_HEIGHT, &dstRightBar, rightBar.getX(), rightBar.getY());
+    putDimensions(&srcBall, BALL_WIDTH, BALL_HEIGHT, &dstBall, ball.getX(), ball.getY());
 
     if(SDL_BlitSurface(leftBar.getSurf(), &srcLeftBar, gScreenSurface, &dstLeftBar) < 0
     || SDL_BlitSurface(rightBar.getSurf(), &srcRightBar, gScreenSurface, &dstRightBar) < 0
-    || SDL_BlitSurface(ball.getSurf(), &srcBall, gScreenSurface, &dstBall) < 0
-  ) {
+    || SDL_BlitSurface(ball.getSurf(), &srcBall, gScreenSurface, &dstBall) < 0) {
       cout << "SDL could not blit! SDL Error: " << SDL_GetError() << endl;
       gQuit = true;
     }
@@ -178,11 +204,17 @@ void startGame() {
 }
 
 void menu() {
-  int cursor = 0;
+  unsigned int cursor = 0;
   SDL_Event e;
+  TTF_Font* font = NULL;
+  SDL_Surface* start, help, exitgame;
+  SDL_Rect dstStart, srcStart, dstHelp, dstExit;
+  SDL_Color color = {255, 255, 255};
 
   if(_DEBUG)
     cout << "[Menu opened]" << endl;
+
+  loadFont(font, 50);
 
   while(!gQuit) {
     while (SDL_PollEvent(&e) != 0) {
@@ -210,16 +242,24 @@ void menu() {
           else if (e.key.keysym.sym == SDLK_DOWN) {
             cursor = (cursor + 1)%3;
             if (_DEBUG) cout << "[Cursor is at pos " << cursor << "]" << endl;
-
           } else if (e.key.keysym.sym == SDLK_UP) {
             cursor = (cursor + 2)%3;
             if (_DEBUG) cout << "[Cursor is at pos " << cursor << "]" << endl;
           }
-          // lots of things to add here
           break;
       }
     }
     SDL_FillRect(gScreenSurface, NULL, SDL_MapRGB(gScreenSurface->format, 0x00, 0x00, 0x00));
+
+    putDimensions(&srcStart, 50, 150, &dstStart, WINDOW_WIDTH/2-75, 150);
+
+    start = loadText(font, "Start", color);
+
+    if(SDL_BlitSurface(start, &srcStart, gScreenSurface, &dstStart) < 0) {
+      cout << "SDL could not blit! SDL Error: " << SDL_GetError() << endl;
+      gQuit = true;
+    }
+
     SDL_UpdateWindowSurface(gWindow);
   }
 }
--- src/functions.h
+++ src/functions.h
@@ -2,9 +2,10 @@
 #define FUNCTIONS_H
 
 bool init();
-SDL_Surface* getMedia(std::string pwd);
+SDL_Surface* getMedia(std::string);
 bool loadMedia();
 void closeAll();
+SDL_Surface* loadText(TTF_Font, std::string, SDL_Color);
 void putDimensions(SDL_Rect*, int, int, int, int, SDL_Rect*, int, int);
 void startGame();
 void menu();
